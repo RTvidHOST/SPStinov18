d2b543ddb11c0935e470ee1201ca7b4353cf402a
RTvidHOST <77573582+RTvidHOST@users.noreply.github.com>
2021-12-02 21:31:22 +0300

    Delete play_music_and_download directory

--- play_music_and_download/src/com/company/Constructor.java
+++
@@ -1,5 +0,0 @@
-package com.company;
-
-public @interface Constructor {
-    String forClass();
-}
--- play_music_and_download/src/com/company/Method.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-import java.lang.annotation.*;
-
-@Target(value = ElementType.METHOD)
-@Documented
-@Retention(value = RetentionPolicy.SOURCE)
-public @interface Method { // аннотация обозначения метода
-}
